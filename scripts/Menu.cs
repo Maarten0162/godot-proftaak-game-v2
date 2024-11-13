1c2a18f55b5dc65875d360569c47cbb12c54124f
Tygonosaurus <tygodukker@gmail.com>
2024-11-13 13:56:43 +0100

    Main Menu

---
+++ scripts/Menu.cs
@@ -0,0 +1,83 @@
+using Godot;
+using Godot.NativeInterop;
+using System;
+using System.Numerics;
+using System.Threading;
+
+public partial class Menu : Node2D
+{
+	Button start = new Button();
+	Button ktwee = new Button();
+	Button kdrie = new Button();
+	Button kvier = new Button();
+	int Speler = 0;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AddChild(start);
+		AddChild(ktwee);
+		AddChild(kdrie);
+		AddChild(kvier);	
+
+		start.Position = new Godot.Vector2(-75, 2);
+		ktwee.Position = new Godot.Vector2(-80, -70);
+		kdrie.Position = new Godot.Vector2(-35, -70);
+		kvier.Position = new Godot.Vector2(10, -70);
+
+
+		start.Text = "Start Game!";		
+		ktwee.Text = "2";
+		kdrie.Text = "3";
+		kvier.Text = "4";
+
+		start.ZIndex = 3;
+		ktwee.ZIndex = 3;
+		kdrie.ZIndex = 3;
+		kvier.ZIndex = 3;
+
+		start.Pressed += ()=> StartGame(Speler);
+		ktwee.Pressed += ()=> twee();
+		kdrie.Pressed += ()=> drie();
+		kvier.Pressed += ()=> vier();
+
+ 	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{	}
+	private void StartGame(int Speler)
+	{
+		if(Speler == 0)
+		{
+			GD.Print("Selecteer het aantal spelers");
+		}
+		else
+		{
+			GetTree().ChangeSceneToFile("res://scenes/main.tscn");
+		}
+		
+	}
+	private void twee()
+	{
+		ktwee.Position = new Godot.Vector2(-80000, -70000);
+		kdrie.Position = new Godot.Vector2(-350000, -70000);
+		kvier.Position = new Godot.Vector2(100000, -70000);
+		Speler = 2;
+		
+	}
+	private void drie()
+	{
+		ktwee.Position = new Godot.Vector2(-80000, -70000);
+		kdrie.Position = new Godot.Vector2(-350000, -70000);
+		kvier.Position = new Godot.Vector2(100000, -70000);
+		Speler = 3;
+	}
+	private void vier()
+	{
+		ktwee.Position = new Godot.Vector2(-80000, -70000);
+		kdrie.Position = new Godot.Vector2(-350000, -70000);
+		kvier.Position = new Godot.Vector2(100000, -70000);
+		Speler = 4;
+	}
+}
