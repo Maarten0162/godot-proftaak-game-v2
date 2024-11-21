c0703712497b598d4615fb33227b79aca5e1b4a5
GuusvanMeel <vanmeelguus@gmail.com>
2024-11-21 09:07:51 +0100

    endscreen

--- scripts/End_Screen.cs
+++ scripts/End_Screen.cs
@@ -3,13 +3,26 @@ using System;
 
 public partial class End_Screen : Node
 {	Label winnerlabel;
+	string winner;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
+	{	int mosthealth = 0;
+		for(int i = 0; i <  GlobalVariables.Instance.playersalive.Count; i++)
+		{
+			if(GlobalVariables.Instance.playersalive[i].Health > mosthealth){
+				GlobalVariables.Instance.playersalive[i].Health = mosthealth;
+			}
+
+		}
+		for(int i = 0; i <  GlobalVariables.Instance.playersalive.Count; i++)
+		{if(GlobalVariables.Instance.playersalive[i].Health == mosthealth){
+			winner = GlobalVariables.Instance.playersalive[i].Name;
+		}
+
 		winnerlabel = GetNode<Label>("WinnerLabel");
-		winnerlabel.Text = "The winner of the game is: " + GlobalVariables.Instance.playersalive[0].Name + "!!!";
-	}
+		winnerlabel.Text = "The winner of the game is: " + winner + "!!!";
+	}}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
